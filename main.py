dafc4c6ce363e3a8ca209d704600a792e219cb91
Muhammed Ashhar <144307824+muhammedashharps@users.noreply.github.com>
2023-12-02 14:06:03 +0530

    Update main.py

--- main.py
+++ main.py
@@ -6,6 +6,15 @@ import datetime
 import pygsheets
 import os
 
+hide_streamlit_style = """
+<style>
+#MainMenu {visibility: hidden;}
+footer {visibility: hidden;}
+</style>
+
+"""
+st.markdown(hide_streamlit_style, unsafe_allow_html=True)
+
 
 # Initialize pygsheets and wks object outside the main block
 service_file = os.environ.get("service_account_file", "haclathons-c80937c3ef59.json")
@@ -119,3 +128,5 @@ if __name__ == "__main__":
     # Display footer message
     with st.container():
         st.markdown("###### Made with :heart: by @ashhar, @rishalali, @anas, @akeel ")
+
+
